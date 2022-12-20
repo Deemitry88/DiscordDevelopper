736cd18c4286694dc0c0f83169ab1254a5ab284b
Deemitry88 <91915462+Deemitry88@users.noreply.github.com>
2022-12-20 21:54:35 +0100

    Add files via upload

---
+++ DiscordDevelopper.plugin.js
@@ -0,0 +1,26 @@
+/**
+ * @name Discord Developper
+ * @author Deemitry88
+ * @description Enable Developper Options
+ * @version 0.0.2
+*/
+
+module.exports = meta => {
+  return {
+    start: () => {
+        let wpRequire;
+        window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
+        mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+        usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+        nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
+        try {
+            nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})
+        } catch (e) {}
+        oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+        usermod.exports.default.__proto__.getCurrentUser = () => ({isStaff: () => true})
+        nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+        usermod.exports.default.__proto__.getCurrentUser = oldGetUser
+    },
+    stop: () => {}
+  }
+};
